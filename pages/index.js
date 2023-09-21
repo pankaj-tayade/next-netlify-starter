4f328ba7bba395f6aab2222d252cc15f059ea90c
pankaj-tayade <71860358+pankaj-tayade@users.noreply.github.com>
2023-09-21 15:24:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Pankaj and Malini Websites!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to Pankaj and Malini Websites <code>pages/index.js</code>
         </p>
       </main>
 
